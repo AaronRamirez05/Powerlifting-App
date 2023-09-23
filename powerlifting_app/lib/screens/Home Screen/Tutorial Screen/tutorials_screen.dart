3cb43a296986ae0a968a771e8298f84251c7169c
AaronRamirez05 <aaronramirez879@yahoo.com>
2023-09-23 02:31:41 -0500

    Finished refactoring tutorial screen, while also setting up to take in database

--- powerlifting_app/lib/screens/Home Screen/Tutorial Screen/tutorials_screen.dart
+++ powerlifting_app/lib/screens/Home Screen/Tutorial Screen/tutorials_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:powerlifting_app/screens/Home Screen/home_screen.dart';
 import 'package:youtube_player_flutter/youtube_player_flutter.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:powerlifting_app/utils/Utils.dart';
 
 class TutorialScreen extends StatefulWidget {
   @override
@@ -9,164 +10,148 @@ class TutorialScreen extends StatefulWidget {
 }
 
 class _Tutorial extends State<TutorialScreen> {
-  late YoutubePlayerController _controller;
-  late YoutubePlayerController _controller2;
-  late YoutubePlayerController _controller3;
+  YoutubeVids temp = new YoutubeVids();
 
   @override
   void initState() {
     super.initState();
-    const url = 'https://www.youtube.com/shorts/8060FZiT5TA';
-    const url2 = 'https://www.youtube.com/shorts/9SdwTIG6hJY';
-    const url3 = 'https://www.youtube.com/shorts/PKsWnUXcYSQ';
 
-    _controller = YoutubePlayerController(
-        initialVideoId: YoutubePlayer.convertUrlToId(url)!,
-        flags: YoutubePlayerFlags(autoPlay: false));
-
-    _controller2 = YoutubePlayerController(
-        initialVideoId: YoutubePlayer.convertUrlToId(url2)!,
-        flags: YoutubePlayerFlags(autoPlay: false));
-
-    _controller3 = YoutubePlayerController(
-        initialVideoId: YoutubePlayer.convertUrlToId(url3)!,
-        flags: YoutubePlayerFlags(autoPlay: false));
+    temp.populateTitleAuthor();
+    temp.populateData();
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        backgroundColor: Colors.black,
-        appBar: AppBar(
-          title: Text(
-            "Tutorial Page",
-            key: Key('tutcheck'),
-            style: TextStyle(
-                color: Colors.black, fontFamily: 'Open', fontSize: 30),
-          ),
-          backgroundColor: Colors.black,
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back),
-            color: Colors.red,
-            iconSize: 35,
-            key: Key('back-cups'),
-            onPressed: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => homeScreen()));
-            },
-          ),
+      backgroundColor: Colors.black,
+      appBar: AppBar(
+        title: Text(
+          "Tutorial Page",
+          key: Key('tutcheck'),
+          style:
+              TextStyle(color: Colors.black, fontFamily: 'Open', fontSize: 30),
         ),
-        body: ListView(
-          children: [
-            Container(
-                padding: EdgeInsets.symmetric(horizontal: 14.0),
-                child: Text(
-                  'Squat Tutorial',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      fontSize: 20, color: Colors.red, fontFamily: 'Open'),
-                )),
-            YoutubePlayer(
-              controller: _controller,
-              showVideoProgressIndicator: true,
-            ),
-            Container(
-                padding: EdgeInsets.symmetric(horizontal: 18.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      'How to Squat Properly (STEP BY STEP)',
-                      style: TextStyle(
-                          fontFamily: 'open',
-                          color: Colors.white,
-                          fontSize: 15),
+        backgroundColor: Colors.black,
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          color: Colors.red,
+          iconSize: 35,
+          key: Key('back-cups'),
+          onPressed: () {
+            Navigator.push(
+                context, MaterialPageRoute(builder: (context) => homeScreen()));
+          },
+        ),
+      ),
+      body: SingleChildScrollView(
+          padding: EdgeInsets.symmetric(horizontal: 20.0),
+          child: Column(
+            children: <Widget>[
+              SizedBox(
+                height: 600,
+                child: ListView(children: [
+                  Container(
+                      padding: EdgeInsets.symmetric(horizontal: 14.0),
+                      child: Text(
+                        'Squat Tutorials',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.red,
+                            fontFamily: 'Open'),
+                      )),
+                  for (int i = 0; i < temp.squat.length; i++) ...[
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: YoutubePlayer(
+                        controller: temp.squat[i],
+                        showVideoProgressIndicator: true,
+                      ),
                     ),
-                    Text(
-                      'Author: Squat University',
-                      style: TextStyle(
-                          fontFamily: 'open',
-                          color: Colors.white,
-                          fontSize: 13),
-                    ),
-                    SizedBox(height:20)
-
+                    if (temp.squatDataTitle.isNotEmpty &&
+                        i < temp.squatDataTitle.length)
+                      Text(temp.squatDataTitle[i],
+                          textAlign: TextAlign.center,
+                          style: TextStyle(
+                              fontSize: 15,
+                              color: Colors.white,
+                              fontFamily: 'Open')),
+                    Text('Author: ${temp.squatDataAuthor[i]}',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 10,
+                            color: Colors.white,
+                            fontFamily: 'Open')),
+                    SizedBox(height: 10)
                   ],
-                )),
-                Container(
-                padding: EdgeInsets.symmetric(horizontal: 14.0),
-                child: Text(
-                  'Bench Tutorial',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      fontSize: 20, color: Colors.red, fontFamily: 'Open'),
-                )),
-                YoutubePlayer(
-              controller: _controller2,
-              showVideoProgressIndicator: true,
-            ),
-            Container(
-                padding: EdgeInsets.symmetric(horizontal: 18.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      'How to: Bench Press',
-                      style: TextStyle(
-                          fontFamily: 'open',
-                          color: Colors.white,
-                          fontSize: 15),
+                  Container(
+                      padding: EdgeInsets.symmetric(horizontal: 14.0),
+                      child: Text(
+                        'Bench Tutorials',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.red,
+                            fontFamily: 'Open'),
+                      )),
+                  for (int i = 0; i < temp.bench.length; i++) ...[
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: YoutubePlayer(
+                        controller: temp.bench[i],
+                        showVideoProgressIndicator: true,
+                      ),
                     ),
-                    Text(
-                      'Author: Squat University',
-                      style: TextStyle(
-                          fontFamily: 'open',
-                          color: Colors.white,
-                          fontSize: 13),
-                    ),
-                    SizedBox(height:10)
-
+                    Text(temp.benchDataTitle[i],
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.white,
+                            fontFamily: 'Open')),
+                    Text('Author: ${temp.benchDataAuthor[i]}',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 10,
+                            color: Colors.white,
+                            fontFamily: 'Open')),
+                    SizedBox(height: 10)
                   ],
-                )),
-                Container(
-                padding: EdgeInsets.symmetric(horizontal: 14.0),
-                child: Text(
-                  'Deadlift Tutorial',
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                      fontSize: 20, color: Colors.red, fontFamily: 'Open'),
-                )),
-                YoutubePlayer(
-              controller: _controller3,
-              showVideoProgressIndicator: true,
-            ),
-            Container(
-                padding: EdgeInsets.symmetric(horizontal: 18.0),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: <Widget>[
-                    Text(
-                      'How To Deadlift: 3 STEPS!',
-                      style: TextStyle(
-                          fontFamily: 'open',
-                          color: Colors.white,
-                          fontSize: 15),
+                  Container(
+                      padding: EdgeInsets.symmetric(horizontal: 14.0),
+                      child: Text(
+                        'Deadlift Tutorials',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 20,
+                            color: Colors.red,
+                            fontFamily: 'Open'),
+                      )),
+                  for (int i = 0; i < temp.Deadlift.length; i++) ...[
+                    ClipRRect(
+                      borderRadius: BorderRadius.circular(20),
+                      child: YoutubePlayer(
+                        controller: temp.Deadlift[i],
+                        showVideoProgressIndicator: true,
+                      ),
                     ),
-                    Text(
-                      'Author: Squat University',
-                      style: TextStyle(
-                          fontFamily: 'open',
-                          color: Colors.white,
-                          fontSize: 13),
-                    ),
-                    SizedBox(height:10)
-
-                  ],
-                )),
-
-          ],
-
-          
-        ));
+                    Text(temp.deadliftTitle[i],
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 15,
+                            color: Colors.white,
+                            fontFamily: 'Open')),
+                    Text('Author: ${temp.deadliftAuthor[i]}',
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                            fontSize: 10,
+                            color: Colors.white,
+                            fontFamily: 'Open')),
+                    SizedBox(height: 10)
+                  ]
+                ]),
+              ),
+            ],
+          )),
+    );
   }
 }
--- powerlifting_app/lib/utils/Utils.dart
+++ powerlifting_app/lib/utils/Utils.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:youtube_data_api/models/video_data.dart';
+import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:powerlifting_app/screens/Home Screen/Tutorial Screen/tutorials_screen.dart';
+import 'package:youtube_explode_dart/youtube_explode_dart.dart';
 
 class Utils {
   static final messengerKey = GlobalKey<ScaffoldMessengerState>();
@@ -10,5 +15,76 @@ class Utils {
     messengerKey.currentState!
       ..removeCurrentSnackBar()
       ..showSnackBar(snackBar);
+  }
+}
+
+class YoutubeVids {
+  String datatemp = "";
+  List<String> Squaturls = [];
+  List<String> Deadlifturls = [];
+  List<String> Benchurls = [];
+
+  List<YoutubePlayerController> squat = [];
+  List<String> squatDataTitle = [];
+  List<String> squatDataAuthor = [];
+  List<YoutubePlayerController> bench = [];
+  List<String> benchDataTitle = [];
+  List<String> benchDataAuthor = [];
+  List<YoutubePlayerController> Deadlift = [];
+  List<String> deadliftTitle = [];
+  List<String> deadliftAuthor = [];
+
+  late YoutubePlayerController _ytController;
+
+  void populateTitleAuthor() {
+    squatDataTitle.add('How to Squat Properly (STEP BY STEP)');
+    squatDataAuthor.add('Squat University');
+    benchDataTitle.add('How to: Bench Press');
+    benchDataAuthor.add('Squat University');
+    deadliftTitle.add('How to Deadlift: 3 STEPS!)');
+    deadliftAuthor.add('Squat University');
+  }
+
+  Future<void> populateData() async {
+    Squaturls.add('https://www.youtube.com/shorts/8060FZiT5TA');
+    Deadlifturls.add('https://www.youtube.com/shorts/PKsWnUXcYSQ');
+    Benchurls.add('https://www.youtube.com/shorts/9SdwTIG6hJY');
+
+    for (int i = 0; i < Squaturls.length; i++) {
+      //Inserts Data into squat controllers
+      String _id = YoutubePlayer.convertUrlToId(Squaturls[i])!;
+
+      _ytController = YoutubePlayerController(
+        initialVideoId: _id,
+        flags: const YoutubePlayerFlags(
+          autoPlay: false,
+        ),
+      );
+      squat.add(_ytController);
+    }
+    //Inserts data into bench controllers
+    for (int i = 0; i < Benchurls.length; i++) {
+      String _id = YoutubePlayer.convertUrlToId(Benchurls[i])!;
+
+      YoutubePlayerController _ytController = YoutubePlayerController(
+        initialVideoId: _id,
+        flags: const YoutubePlayerFlags(
+          autoPlay: false,
+        ),
+      );
+      bench.add(_ytController);
+    }
+
+    //Inserts data into deadlift controllers
+    for (int i = 0; i < Deadlifturls.length; i++) {
+      String _id = YoutubePlayer.convertUrlToId(Deadlifturls[i])!;
+      YoutubePlayerController _ytController = YoutubePlayerController(
+        initialVideoId: _id,
+        flags: const YoutubePlayerFlags(
+          autoPlay: false,
+        ),
+      );
+      Deadlift.add(_ytController);
+    }
   }
 }
