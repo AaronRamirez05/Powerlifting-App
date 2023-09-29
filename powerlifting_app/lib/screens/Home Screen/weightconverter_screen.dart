9c79e17481cc355973e4353190a439572b755403
AaronRamirez05 <aaronramirez879@yahoo.com>
2023-09-29 14:28:54 -0500

    fixed text fields converting

--- powerlifting_app/lib/screens/Home Screen/weightconverter_screen.dart
+++ powerlifting_app/lib/screens/Home Screen/weightconverter_screen.dart
@@ -7,105 +7,131 @@ class ConvertScreen extends StatefulWidget {
 }
 
 class _Convert extends State<ConvertScreen> {
-  final TextEditingController value = TextEditingController();
-  final TextEditingController value2 = TextEditingController();
-  var kilograms, conversion, pounds;
+  TextEditingController value = TextEditingController();
+  TextEditingController value2 = TextEditingController();
+  var kilograms, conversion1, conversion2, pounds;
 
-  void convert1(value) {
+  void convert1(String inputvalue) {
     setState(() {
-      kilograms = int.parse(value.text);
-      conversion = kilograms * 2.20462;
+      pounds = double.tryParse(inputvalue);
+      conversion1 = pounds / 2.20462;
+      value2.text = conversion1.toStringAsFixed(3);
     });
   }
 
-  void convert2(value2) {
+  void convert2(String inputvalue) {
     setState(() {
-      pounds = int.parse(value2.text);
-      conversion = pounds / 2.20462;
+      kilograms = double.tryParse(inputvalue);
+      conversion2 = kilograms * 2.20462;
+      value.text = conversion2.toStringAsFixed(3);
     });
   }
 
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          backgroundColor: Colors.black,
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back),
-            color: Colors.red,
-            iconSize: 35,
-            key: Key('back-cups'),
-            onPressed: () {
-              Navigator.push(context,
-                  MaterialPageRoute(builder: (context) => homeScreen()));
-            },
-          ),
-        ),
-        backgroundColor: Colors.black,
-        body: Container(
-          child: Center(
-              child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 19.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  child: Text(
-                    "Weight Converter",
-                    key:Key('convertcheck'),
-                    style: TextStyle(color: Colors.red, fontFamily: 'Open'),
-                  ),
-                ),
-                SizedBox(height: 20),
-                Container(
-                    color: Colors.white,
-                    height: 50,
-                    width: 200,
-                    child: TextFormField(
-                        controller: value,
-                        keyboardType: TextInputType.number,
-                        decoration: InputDecoration(
-                            hintText: 'Enter the weight in pounds'),
-                        onChanged: (value) {
-                          convert1(value);
-                        },)
-                ),
-                SizedBox(height: 50),
-                Container(
-                    color: Colors.white,
-                    height: 50,
-                    width: 200,
-                    child: TextFormField(
-                        controller: value2,
-                        keyboardType: TextInputType.number,
-                        decoration: InputDecoration(
-                            hintText: 'Enter the weight in kilograms'),
-                        onChanged: (value2) {
-                          convert2(value2);
-                        })
-                ),
-                SizedBox(height: 50),
-                Container(
-                  child: CustomPaint(
-                    foregroundPainter: WeightPainter(),
-                  )
-                )
-              ],
+    return GestureDetector(
+        onTap: () {
+          FocusScope.of(context).unfocus();
+        },
+        child: Scaffold(
+            appBar: AppBar(
+              backgroundColor: Colors.black,
+              leading: IconButton(
+                icon: Icon(Icons.arrow_back),
+                color: Colors.red,
+                iconSize: 35,
+                key: Key('back-cups'),
+                onPressed: () {
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => homeScreen()));
+                },
+              ),
             ),
-          )),
-        ));
+            backgroundColor: Colors.black,
+            body: Container(
+              child: Center(
+                  child: Padding(
+                padding: EdgeInsets.symmetric(horizontal: 19.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Container(
+                      child: Text(
+                        "Weight Converter",
+                        key: Key('convertcheck'),
+                        style: TextStyle(color: Colors.red, fontFamily: 'Open'),
+                      ),
+                    ),
+                    SizedBox(height: 20),
+                    Container(
+                        color: Colors.white,
+                        height: 50,
+                        width: 200,
+                        child: TextFormField(
+                          controller: value,
+                          keyboardType: TextInputType.number,
+                          decoration: InputDecoration(
+                              hintText: 'Enter the weight in pounds'),
+                          onChanged: (value) {
+                            setState(() {
+                              convert1(value);
+                              if (value == "") {
+                                value2.clear();
+                              }
+                            });
+                          },
+                          onEditingComplete: () {
+                            if (value.text.isEmpty) {
+                              value2.clear();
+                            }
+                          },
+                        )),
+                    SizedBox(height: 50),
+                    Container(
+                        color: Colors.white,
+                        height: 50,
+                        width: 200,
+                        child: TextFormField(
+                            controller: value2,
+                            keyboardType: TextInputType.number,
+                            decoration: InputDecoration(
+                                hintText: 'Enter the weight in kilograms'),
+                            onChanged: (value2) {
+                              setState(() {
+                                convert2(value2);
+                                if (value2 == "") {
+                                  value.clear();
+                                }
+                              });
+                            },
+                            onEditingComplete: () {
+                              if (value2.text.isEmpty) {
+                                value.clear();
+                              }
+                            })),
+                    SizedBox(height: 50),
+                    Container(
+                        child: CustomPaint(
+                      foregroundPainter: WeightPainter(),
+                    ))
+                  ],
+                ),
+              )),
+            )));
   }
 }
 
 class WeightPainter extends CustomPainter {
   @override
   void paint(Canvas canvas, Size size) {
-    final weight = Paint()..strokeWidth = 20..color = Colors.red;
+    final weight = Paint()
+      ..strokeWidth = 20
+      ..color = Colors.red;
 
     canvas.drawLine(
-      Offset(size.width * 1/2, size.height * 1/6),
-      Offset(size.width * 1/2, size.height * 2/3),
+      Offset(size.width * 1 / 2, size.height * 1 / 6),
+      Offset(size.width * 1 / 2, size.height * 2 / 3),
       weight,
-      );
+    );
   }
 
   @override
