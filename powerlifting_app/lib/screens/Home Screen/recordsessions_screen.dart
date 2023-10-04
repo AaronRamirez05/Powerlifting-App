75ff9e52cbdbb30f5aeaf29d64a7171ae2edba2d
AaronRamirez05 <aaronramirez879@yahoo.com>
2023-10-03 23:53:34 -0500

    Finished record screen

--- powerlifting_app/lib/screens/Home Screen/recordsessions_screen.dart
+++ powerlifting_app/lib/screens/Home Screen/recordsessions_screen.dart
@@ -1,49 +1,108 @@
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:powerlifting_app/screens/Home Screen/home_screen.dart';
+import 'dart:async';
+import 'package:camera/camera.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
+import 'package:video_player/video_player.dart';
+import 'package:powerlifting_app/screens/Home Screen/video_page.dart';
 
 class RecordScreen extends StatefulWidget {
+  const RecordScreen({super.key});
+
   @override
-  _Record createState() => _Record();
-}
-
-class _Record extends State<RecordScreen> {
-
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-          backgroundColor: Colors.black,
-          leading: IconButton(
-            icon: Icon(Icons.arrow_back),
-            color: Colors.red,
-            iconSize: 35,
-            key: Key('back-cups'),
-            onPressed: () {
-              Navigator.push(
-                  context, MaterialPageRoute(builder: (context) => homeScreen()));
-            },
-          ),
-        ),
-        backgroundColor: Colors.black,
-        body: 
-        Container(
-          child: Center(
-              child: Padding(
-            padding: EdgeInsets.symmetric(horizontal: 19.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Container(
-                  child: Text(
-                    "Record Sessions",
-                    key:Key('recordcheck'),
-                    style: TextStyle(color: Colors.red, fontFamily: 'Open'),
-                  ),
-                ),
-
-              ],
-            ),
-          )),
-        ));
+  State<RecordScreen> createState() {
+    return _Record();
   }
 }
 
+class _Record extends State<RecordScreen> {
+  bool _isLoading = true;
+  late CameraController _cameraController;
+  bool _isRecording = false;
+
+  @override
+  void dispose() {
+    _cameraController.dispose();
+    super.dispose();
+  }
+
+  _initCamera() async {
+    final cameras = await availableCameras();
+    final front = cameras.firstWhere(
+        (camera) => camera.lensDirection == CameraLensDirection.front);
+    _cameraController = CameraController(front, ResolutionPreset.max);
+    await _cameraController.initialize();
+    setState(() => _isLoading = false);
+  }
+
+  _recordVideo() async {
+    if (_isRecording) {
+      final file = await _cameraController.stopVideoRecording();
+      setState(() => _isRecording = false);
+      final route = MaterialPageRoute(
+        fullscreenDialog: true,
+        builder: (_) => VideoPage(filePath: file.path),
+      );
+      Navigator.push(context, route);
+    } else {
+      await _cameraController.prepareForVideoRecording();
+      await _cameraController.startVideoRecording();
+      setState(() => _isRecording = true);
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _initCamera();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (_isLoading) {
+      return Container(
+        color: Colors.black,
+        child: const Center(
+          child: CircularProgressIndicator(
+            color: Colors.red,
+          ),
+        ),
+      );
+    } else {
+      return Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.black,
+            elevation: 0,
+            leading: IconButton(
+              icon: Icon(Icons.arrow_back),
+              color: Colors.red,
+              iconSize: 35,
+              key: Key('back-cups'),
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => homeScreen()));
+              },
+            ),
+          ),
+          body: Center(
+            child: Stack(
+              alignment: Alignment.bottomCenter,
+              children: [
+                CameraPreview(_cameraController),
+                Padding(
+                  padding: const EdgeInsets.all(25),
+                  child: FloatingActionButton(
+                    backgroundColor: Colors.red,
+                    child: Icon(_isRecording ? Icons.stop : Icons.circle),
+                    onPressed: () => _recordVideo(),
+                  ),
+                ),
+              ],
+            ),
+          ));
+    }
+  }
+}
